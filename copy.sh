da077b486fb524fab94adb6810b5fdb8ae20ffc3
Keiko Nakata <kk.nkt33@gmail.com>
2021-06-23 00:04:13 +0200

    .

--- copy.sh
+++ copy.sh
@@ -2,8 +2,8 @@
 
 cp /home/keiko/projects/my-site/_site/*.html /home/keiko/repos/keikonakata.github.io/
 
-cp /home/keiko/projects/my-site/_site/*.html /home/keiko/repos/keikonakata.github.io/
-
 cp /home/keiko/projects/my-site/_site/css/default.css /home/keiko/repos/keikonakata.github.io/css
 
 cp /home/keiko/projects/my-site/_site/crypto/*.html /home/keiko/repos/keikonakata.github.io/crypto/
+
+cp /home/keiko/projects/my-site/_site/distr/*.html /home/keiko/repos/keikonakata.github.io/distr/
